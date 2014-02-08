580a5931362528e113cbefc667b12aff39137871
Dario Salvi <dariosalvi78@gmail.com>
2014-02-08 18:29:23 +0100

    added Services.h

---
+++ Services.h
@@ -0,0 +1,22 @@
+#ifndef SERVICES_h
+#define SERVICES_h
+
+#include "Sensorino.h"
+
+
+
+class Sensorino
+{
+    public:
+
+    protected:
+    private:
+
+
+};
+
+/** THE instance, Arduino style
+ */
+extern Services service;
+
+#endif // SERVICES_h
