d2f1266ee2ef3b7543b6baaa0e0f33f49cb25b17
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2014-05-18 07:07:38 +0200

    Missing method declaration (implementation WIP).

--- Sensorino/Sensorino.h
+++ Sensorino/Sensorino.h
@@ -21,10 +21,19 @@ class Sensorino
         void addService(Service *s);
         void deleteService(Service *s);
 
-	Service *getServiceById(int id);
-	Service *getServiceByNum(int num);
+        Service *getServiceById(int id);
+        Service *getServiceByNum(int num);
 
-	static void die(const char *err = NULL) __attribute__((noreturn));
+        static void die(const char *err = NULL) __attribute__((noreturn));
+
+        /* TODO:
+         * Since the Atmega only has one IRQ line per port (8 GPIOs)
+         * this function lets a Service handle individual GPIO interrupts
+         * by providing a group handler that calls individual handlers as
+         * needed.
+         */
+        void attachGPIOInterrupt(int pin, void (*handler)(int pin, void *data),
+                void *data);
 
     private:
         uint8_t address;
