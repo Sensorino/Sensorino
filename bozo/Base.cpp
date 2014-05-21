e54970bef7d29830bbd082fe2a3c5ce83f2c8bba
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2014-05-21 12:18:33 +0200

    Primitive Base implementation.

--- bozo/Base.cpp
+++ bozo/Base.cpp
@@ -1,13 +1,93 @@
-#include <Base.h>
+#include <RHReliableDatagram.h>
+#include <RH_NRF24.h>
 
+#include "Base.h"
+#include "MessageJsonConverter.h"
 
-void Base::run(){
-    listen(listenTimeout);
+/* TODO: make these configurable */
+#define CONFIG_CSN_PIN  14
+#define CONFIG_CE_PIN   15
+#define CONFIG_INTR_PIN 10
+
+/* TODO: put all the utils in a header */
+#ifndef unlikely
+# define unlikely(x)	__builtin_expect((x), 0)
+#endif
+
+MessageJsonConverter conv;
+
+RH_NRF24 radio(CONFIG_CE_PIN, CONFIG_CSN_PIN);
+RHReliableDatagram radioManager(radio, 0);
+
+void Base::setup() {
+    Serial.begin(115200);
+
+    PCMSK0 = 0;
+    PCMSK1 = 0;
+    PCMSK2 = 0;
+    PCICR = 0;
+
+    pinMode(CONFIG_INTR_PIN, INPUT);
+    *digitalPinToPCMSK(CONFIG_INTR_PIN) |=
+        1 << digitalPinToPCMSKbit(CONFIG_INTR_PIN);
+    *digitalPinToPCICR(CONFIG_INTR_PIN) |=
+        1 << digitalPinToPCICRbit(CONFIG_INTR_PIN);
+
+    sei();
 }
 
+void Base::loop() {
+    /* Wait until something happens on UART or radio */
+    __asm__ volatile ("sleep");
 
-void onSerialMessage(){
+    /* We've received and parsed a new JSON message */
+    if (conv.obj) {
+        Message *msg = MessageJsonConverter::jsonToMessage(*conv.obj);
+        aJson.deleteItem(conv.obj);
+        conv.obj = NULL;
 
+        /* Succesfully converted to Message */
+        if (msg) {
+            radioManager.sendtoWait((uint8_t *) msg->getRawData(),
+                    msg->getRawLength(), msg->getDstAddress());
+            delete msg;
+        }
+    }
 
+    while (radioManager.available()) {
+        uint8_t pkt[RH_NRF24_MAX_MESSAGE_LEN], len;
+
+        /* New radio packet received */
+        if (radioManager.recvfromAck(pkt, &len, NULL, NULL, NULL, NULL)) {
+            Message msg(pkt, len);
+            aJsonObject *obj = MessageJsonConverter::messageToJson(msg);
+
+            /* Successfully converted to JSON */
+            if (obj) {
+                /* FIXME this blocks */
+                char *str = aJson.print(obj);
+                Serial.write(str);
+                free(str);
+            }
+        }
+    }
 }
 
+ISR(USART_RX_vect) {
+    uint8_t status = UCSR0A;
+    uint8_t ch = UDR0;
+
+    if (unlikely(status & 0x14))
+        return;
+
+    conv.putch(ch);
+}
+
+ISR(PCINT0_vect) {
+}
+ISR(PCINT1_vect) {
+}
+ISR(PCINT2_vect) {
+}
+
+/* vim: set sw=4 ts=4 et: */
--- bozo/Base.h
+++ bozo/Base.h
@@ -1,36 +1,10 @@
 #ifndef BASE_H
 #define BASE_H
 
-
-//Maximum number of instantiable base services (excluding control ones)
-#define MAX_SERVICES 20
-
-class Base
-{
-    public:
-        void run();
-    
-        // stuff received from radio
-        void onSerialJsonMessage();
-        void onRadioMessage();
-        
-
-    private: 
-
-        Service[MAX_SERVICES] services;
-
-        int listenTimeout=5;
-        void listen(int timeout);
-
-        void onSerialEvent();
-
-        // stuff received from serial
-        void onSerialRequest();
-        void onSerialSet();
-        void onSerialPing();
-        void onSerialPong();
-
-}
-
+class Base {
+public:
+    static void setup(void);
+    static void loop(void);
+};
 
 #endif // whole file
