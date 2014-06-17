4a0df6ef2d55aa245a6acc0c29ffb8f0fb83fb68
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2014-06-17 22:25:03 +0200

    Replicate RH's recvfromAck blocking behavior.
    The FragmentedDatagram class temporarily relies on this behavior so
    we've got to implement it too.

--- Sensorino/mini-radiohead.cpp
+++ Sensorino/mini-radiohead.cpp
@@ -426,6 +426,8 @@ bool RHReliableDatagram::sendtoWait(uint8_t *buf, uint8_t len,
 
 bool RHReliableDatagram::recvfromAck(uint8_t *buf, uint8_t *len, uint8_t *from,
 		uint8_t *to, uint8_t *id, uint8_t *flags) {
+	while (!available());
+
 	nrf24_rx_read(buf, len);
 
 	return 1;
